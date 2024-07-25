db2be2fca8c8df772bf744ea1b068f660cc38aca
William Chang <wilicw1346@gmail.com>
2024-07-25 17:19:49 +0800

    fix: FSAE 2024 rule EV.4.7.1

--- Tasks/Src/control.c
+++ Tasks/Src/control.c
@@ -16,10 +16,14 @@ Revision: $Rev: 2024.11$
 #include "stm32f4xx_hal_gpio.h"
 #include "tx_port.h"
 
+#define IS_PRECHARGED (recv_events_flags & EVENT_BIT(EVENT_PRECHARGE))
+#define IS_RECORDED (recv_events_flags & EVENT_BIT(EVENT_LOGGING))
+
 static const float TORQUE_FACTOR = 10.0f;
 static const float MAX_TORQUE = 150 * TORQUE_FACTOR;
 static const float REGEN_TORQUE = (REGEN_ENABLE ? -30 : 0) * TORQUE_FACTOR;
 static const float BSE_MAX = 75.0f;
+const static float MAX_PEDAL_POSITION = MAX_TORQUE - REGEN_TORQUE;
 
 TX_THREAD control_thread;
 extern TX_EVENT_FLAGS_GROUP event_flags;
@@ -38,10 +42,10 @@ static inline void control_stopped() {
 
   static const float CALIBRATION_APPS = 0.1 * MAX_TORQUE;
   static const float RTD_BPPS = 50.0f;
-  uint8_t apps_triggered =
+  const uint8_t apps_triggered =
       apps_l->value < -CALIBRATION_APPS && apps_r->value > CALIBRATION_APPS;
-  uint8_t bpps_triggered = bpps_l->value > RTD_BPPS && bpps_r->value > RTD_BPPS;
-  uint8_t precharge_complete = recv_events_flags & EVENT_BIT(EVENT_PRECHARGE);
+  const uint8_t bpps_triggered =
+      bpps_l->value > RTD_BPPS && bpps_r->value > RTD_BPPS;
 
   HAL_GPIO_WritePin(RTD_OUTPUT_GPIO_Port, RTD_OUTPUT_Pin, GPIO_PIN_RESET);
 
@@ -56,7 +60,6 @@ static inline void control_stopped() {
 
 static inline void control_calibrate() {
   // Calibrate the APPS and Steering wheel sensors
-  const static float MAX_PEDAL_POSITION = MAX_TORQUE - REGEN_TORQUE;
   apps_l->cal.scale *= -MAX_PEDAL_POSITION / apps_l->value;
   apps_r->cal.scale *= MAX_PEDAL_POSITION / apps_r->value;
 
@@ -79,60 +82,65 @@ static inline void control_rtd() {
 }
 
 static inline void control_running() {
-  /* NOTE: If precharge pin goes low (TS is below 60V), disable the torque
-           output.
-  */
-  // if (!(recv_events_flags & EVENT_BIT(EVENT_PRECHARGE))) {
-  //   inverter_R->torque = 0;
-  //   inverter_L->torque = 0;
-  //   return;
-  // }
+  const float __COMMAND_TORQUE =
+      ((-apps_l->value + apps_r->value) / 2 + REGEN_TORQUE) / TORQUE_FACTOR;
+  const float COMMAND_TORQUE =
+      __COMMAND_TORQUE > MAX_TORQUE
+          ? MAX_TORQUE
+          : (__COMMAND_TORQUE < REGEN_TORQUE ? REGEN_TORQUE : __COMMAND_TORQUE);
+  const float COMMAND_BREAK = (bpps_l->value + bpps_r->value) / 2;
 
-  /* NOTE:
-   * T.4.3.3 (...skip) Any failure of the BSE or BSE wiring that persists
-   * more than 100 msec must be detectable by the controller and treated like an
-   * implausibility and power to the (IC) electronic throttle / (EV) Motor(s)
-   * must be immediately stopped completely.
-   * -
-   * T.4.3.4 When an analogue signal is used, the BSE sensors will be considered
-   * to have failed when they achieve an open circuit or short circuit condition
-   * which generates a signal outside of the normal operating range, for example
-   * <0.5 V or >4.5 V.
-   */
+  /* WARN: BYPASS the rule while recoding */
+  if (!IS_RECORDED) {
+    /* NOTE:
+     * T.4.3.3 (...skip) Any failure of the BSE or BSE wiring that persists
+     * more than 100 msec must be detectable by the controller and treated like
+     * an implausibility and power to the (IC) electronic throttle / (EV)
+     * Motor(s) must be immediately stopped completely.
+     * -
+     * T.4.3.4 When an analogue signal is used, the BSE sensors will be
+     * considered to have failed when they achieve an open circuit or short
+     * circuit condition which generates a signal outside of the normal
+     * operating range, for example <0.5 V or >4.5 V.
+     */
 
-  if (bpps_l->value >= bpps_r->value * 1.1f ||
-      bpps_r->value >= bpps_l->value * 1.1f || bpps_l->value < -1 ||
-      bpps_r->value < -1 || bpps_l->value > BSE_MAX * 1.1f ||
-      bpps_r->value > BSE_MAX * 1.1f) {
-    inverter_R->torque = 0;
-    inverter_L->torque = 0;
-    return;
+    if (bpps_l->value >= bpps_r->value * 1.1f ||
+        bpps_r->value >= bpps_l->value * 1.1f || bpps_l->value < -1 ||
+        bpps_r->value < -1 || bpps_l->value > BSE_MAX * 1.1f ||
+        bpps_r->value > BSE_MAX * 1.1f) {
+      inverter_R->torque = inverter_L->torque = 0;
+      return;
+    }
+
+    /* NOTE: EV.4.7.1 Must monitor for the two conditions:
+     *        • The mechanical brakes are engaged EV.4.6, T.3.2.4
+     *        • The APPS signals more than 25% Pedal Travel EV.4.5
+     * -
+     * EV.4.7.2 If the two conditions in EV.4.7.1 occur at the same time:
+     *   a. Power to the Motor(s) must be immediately and completely shut down
+     *   b. The Motor shut down must stay active until the APPS signals less
+     * than 5% Pedal Travel, with or without brake operation */
+    static uint8_t ev471_triggered = 0;
+    if (!ev471_triggered && COMMAND_BREAK > 25 &&
+        COMMAND_TORQUE >= MAX_TORQUE * 0.15f) {
+      inverter_R->torque = inverter_L->torque = 0;
+      ev471_triggered = 1;
+    }
+    if (ev471_triggered && COMMAND_TORQUE <= MAX_TORQUE * 0.05f) {
+      ev471_triggered = 0;
+    }
   }
 
-  /* NOTE: Disable the torque output if the APPS is less than 5% of the maximum
-           torque output.
+  /* NOTE: Disable the torque output if the APPS is less than 5% of the
+     maximum torque output.
   */
   if (apps_l->value >= -MAX_TORQUE * 0.05f &&
       apps_r->value <= MAX_TORQUE * 0.05f) {
-    inverter_R->torque = 0;
-    inverter_L->torque = 0;
+    inverter_R->torque = inverter_L->torque = 0;
     return;
   }
 
-  /* TODO: EV.3.3.3 The powertrain must not regenerate energy when vehicle speed
-   *       is between 0 and 5 km/hr
-   */
-
-  inverter_R->torque = inverter_L->torque =
-      ((-apps_l->value + apps_r->value) / 2 + REGEN_TORQUE) / TORQUE_FACTOR;
-  inverter_R->torque =
-      (inverter_R->torque > MAX_TORQUE) ? MAX_TORQUE : inverter_R->torque;
-  inverter_L->torque =
-      (inverter_L->torque > MAX_TORQUE) ? MAX_TORQUE : inverter_L->torque;
-  inverter_R->torque =
-      (inverter_R->torque < REGEN_TORQUE) ? REGEN_TORQUE : inverter_R->torque;
-  inverter_L->torque =
-      (inverter_L->torque < REGEN_TORQUE) ? REGEN_TORQUE : inverter_L->torque;
+  inverter_R->torque = inverter_L->torque = COMMAND_TORQUE;
 }
 
 void control_thread_entry(ULONG thread_input) {
