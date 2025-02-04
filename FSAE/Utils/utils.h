21622bbcb7369b8213a57fbb6ef665c31b52994c
William Chang <wilicw1346@gmail.com>
2025-02-04 16:30:24 +0800

    feat: implement abs and min

--- FSAE/Utils/utils.h
+++ FSAE/Utils/utils.h
@@ -7,4 +7,8 @@
 #define INTERVAL_16U(current_time, last_time) \
   (((0xFFFF - (last_time)) + (current_time)) + 1)
 
+#define _ABS_(x) ((x) < 0 ? -(x) : (x))
+
+#define _MIN_(a, b) ((a) < (b) ? (a) : (b))
+
 #endif
